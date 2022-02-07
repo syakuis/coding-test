c2715bd93c9db1191f0cf2f712288a959484e51b
syaku <syaku@naver.com>
2022-02-07 23:29:26 +0900

    두합 알고리즘 구현 추가

---
+++ src/main/java/io/github/syakuis/coding/com/leetcode/problems/TwoSum.java
@@ -0,0 +1,46 @@
+package io.github.syakuis.coding.com.leetcode.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Seok Kyun. Choi.
+ * @since 2022-02-07
+ */
+public class TwoSum {
+    /**
+     * 일반적인 구현 : 가장 느림
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] basic(int[] nums, int target) {
+        int count = nums.length;
+        for(int i = 0; i < count; i++) {
+            for(int x = i + 1; x < count; x++) {
+                int sum = nums[i] + nums[x];
+                if (sum == target) {
+                    return new int[]{i,x};
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public int[] map(int[] nums, int target) {
+        Map<Integer, int[]> store = new HashMap<>();
+
+        int count = nums.length;
+        for(int i = 0; i < count; i++) {
+            for(int x = i + 1; x < count; x++) {
+                int sum = nums[i] + nums[x];
+                if (!store.containsKey(sum)) {
+                    store.put(sum, new int[]{i, x});
+                }
+            }
+        }
+
+        return store.get(target);
+    }
+}
