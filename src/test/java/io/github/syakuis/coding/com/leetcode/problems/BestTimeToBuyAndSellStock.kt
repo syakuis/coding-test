923f3a5dd4ab3e0dc4d799242202f5f8b4035fb4
syaku <syaku@naver.com>
2022-06-07 20:44:30 +0900

    12 주식을 사고팔기 가장 좋은 시점

--- src/test/java/io/github/syakuis/coding/com/leetcode/problems/BestTimeToBuyAndSellStock.kt
+++ src/test/java/io/github/syakuis/coding/com/leetcode/problems/BestTimeToBuyAndSellStock.kt
@@ -2,6 +2,8 @@ package io.github.syakuis.coding.com.leetcode.problems
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * @author Seok Kyun. Choi.
@@ -10,7 +12,7 @@ import io.kotest.matchers.shouldBe
 class BestTimeToBuyAndSellStock: DescribeSpec({
     describe("주식을 사고팔기 가장 좋은 시점") {
         describe("한 번의 거래로 낼 수 있는 최대 이익을 산출하라.") {
-            val nums = intArrayOf(7, 1, 5, 3, 6, 4)
+            val nums = intArrayOf(1, 2, 3, 0, 2)
 
             it ("저점과 현재 값과의 차이 계산") {
                 fun solution(nums: IntArray): Int {
@@ -18,14 +20,15 @@ class BestTimeToBuyAndSellStock: DescribeSpec({
                     var minPrice = Int.MAX_VALUE
 
                     for (num in nums) {
-                        minPrice = minOf(minPrice, num)
-                        profit = maxOf(profit, num - minPrice)
+                        minPrice = min(minPrice, num)
+                        profit = max(profit, num - minPrice)
+                        println("$num $minPrice $profit")
                     }
 
                     return profit
                 }
 
-                solution(nums) shouldBe 5
+                solution(nums) shouldBe 3
             }
         }
     }
